8d7d11cad5dfd7388f2c1311093278a12f663b82
Steve McNamara <stephenmcnamara36@gmail.com>
2021-03-05 10:08:17 -0700

    added necerssary handlebars files

--- controllers/patients_controller.js
+++ controllers/patients_controller.js
@@ -1,10 +1,20 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+const passport = require("../config/passport");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 router.get("/", function (req, res) {
   // send us to the next get function instead.
   res.redirect("/patients");
+});
+
+router.get("/", function (req, res) {
+  // If the user already has an account send them to the members page
+  if (req.user) {
+    res.redirect("/patients");
+  }
+  res.render("signup");
 });
 
 router.get("/patients/create", function (req, res) {
@@ -15,12 +25,16 @@ router.get("/patients/create", function (req, res) {
 // get route, edited to match sequelize
 router.get("/patients", function (req, res) {
   // replace old function with sequelize function
-  db.Patient.findAll().then(function (dbPatient) {
-    console.log(dbPatient);
-    // into the main index, updating the page
-    var hbsObject = { patient: dbPatient };
-    return res.render("index", hbsObject);
-  });
+  if (req.user) {
+    db.Patient.findAll().then(function (dbPatient) {
+      console.log(dbPatient);
+      // into the main index, updating the page
+      const hbsObject = { patient: dbPatient };
+      return res.render("index", hbsObject);
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 // post route to create burgers
@@ -59,67 +73,59 @@ router.post("/patients/create", function (req, res) {
 //     });
 // });
 
- router.post("/api/login", passport.authenticate("local"), function (req, res) {
-   res.json(req.user);
- });
+router.post("/api/login", passport.authenticate("local"), function (req, res) {
+  res.json(req.user);
+});
 
- // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
- // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
- // otherwise send back an error
- router.post("/api/signup", function (req, res) {
-   db.User.create({
-     email: req.body.email,
-     password: req.body.password,
-   })
-     .then(function () {
-       res.redirect(307, "/api/login");
-     })
-     .catch(function (err) {
-       res.status(401).json(err);
-     });
- });
+// Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+// how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+// otherwise send back an error
+router.post("/api/signup", function (req, res) {
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then(function () {
+      res.redirect(307, "/api/login");
+    })
+    .catch(function (err) {
+      res.status(401).json(err);
+    });
+});
 
- // Route for logging user out
- router.get("/logout", function (req, res) {
-   req.logout();
-   res.redirect("/");
- });
+// Route for logging user out
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
- // Route for getting some data about our user to be used client side
- router.get("/api/user_data", function (req, res) {
-   if (!req.user) {
-     // The user is not logged in, send back an empty object
-     res.json({});
-   } else {
-     // Otherwise send back the user's email and id
-     // Sending back a password, even a hashed password, isn't a good idea
-     res.json({
-       email: req.user.email,
-       id: req.user.id,
-     });
-   }
- });
-
-router.get("/", function (req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/members");
+// Route for getting some data about our user to be used client side
+router.get("/api/user_data", function (req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id,
+    });
   }
-  res.render('signup');
 });
 
 router.get("/login", function (req, res) {
   // If the user already has an account send them to the members page
   if (req.user) {
-    res.redirect("/members");
+    res.redirect("/patients");
   }
-  res.render('login');
+  res.render("login");
 });
 
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get("/members", isAuthenticated, function (req, res) {
-  res.redirect('/patients'));
+  res.redirect("/patients");
 });
 
 module.exports = router;
