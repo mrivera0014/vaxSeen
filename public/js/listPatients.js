ef504291242e51ac9942b06bcf6c9fa9b6b0a280
Marble <mrivera0014@gmail.com>
2021-03-07 16:21:39 -0700

    got add patient button to work

--- public/js/listPatients.js
+++ public/js/listPatients.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-    $(".btn").on("click",function(event) {
-          $.get("/patients/create", {
-            
-          }).then( function() {
-            window.location.assign("/patients");
-            //   console.log("changed vaccinated to", newVaccinated);
-              // Reload the page to get the updated list
-            
-            })
-          .catch(function(err) {
-            console.log(err);
-          });
-        
-        });
+$(document).ready(function () {
+  $(".btn").on("click", function (event) {
+    event.preventDefault()
+    $.get("/patients/create", {
+
+    }).then(function () {
+      window.location.assign("/patients/create");
+      //   console.log("changed vaccinated to", newVaccinated);
+      // Reload the page to get the updated list
+
+    })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+  });
 });
 
 
